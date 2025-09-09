c47671776e422c68ea7421fb7432e960afd1c3aa
Giovanni Donati <grdonati99@gmail.com>
2025-09-09 09:53:08 -0300

    feat: up the started backend-worker, with a docker-compose configuration to rabbitmq

---
+++ backend-worker/src/test/java/com/gdonati/backend_worker/BackendWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdonati.backend_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
